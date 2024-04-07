e37e962f2d7bfcc6135b1342adbc9cdf5d2a8b33
Rodrigo Menéndez Muñoz <rodmenm@gmail.com>
2024-04-08 00:18:56 +0200

    Redireccion_correction

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -396,7 +396,7 @@ exports.objchan = async (req, res, next) => {
     ejer;
   await axios.get(http);
   //Realizar llamada a la api para guardarlos
-  res.redirect("/calendario");
+  res.redirect("/seguimiento");
 };
 
 exports.segsav = async (req, res, next) => {
@@ -423,7 +423,7 @@ exports.segsav = async (req, res, next) => {
   await axios.get(http);
   // El tiempo lo guardaba en minutos
   //Realizar llamada a la api para guardarlos
-  res.redirect("/calendario");
+  res.redirect("/seguimiento");
 };
 
 exports.segsaveini = async (req, res, next) => {
@@ -450,5 +450,5 @@ exports.segsaveini = async (req, res, next) => {
   await axios.get(http);
   // El tiempo lo guardaba en minutos
   //Realizar llamada a la api para guardarlos
-  res.redirect("/calendario");
+  res.redirect("/seguimiento");
 };
