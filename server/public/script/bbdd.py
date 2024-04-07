341d4f760a7ecec2d6466f2c810adc8af21f7d1a
jperez022 <jorge.perez.pineiro@gmail.com>
2024-04-08 00:40:31 +0200

    New petitions for platos sugeridos

--- server/public/script/bbdd.py
+++ server/public/script/bbdd.py
@@ -231,9 +231,29 @@ def obtener_objetivos(usuario):
     resp = {"peso_ini": elem.peso_ini, "peso_act": elem.peso_act, "peso_obj": elem.peso_obj, "ejercicio_act": elem.ejercicio_act, "ejercicio_obj": elem.ejercicio_obj}
     return Response(json.dumps(resp),200)
 
-@app.route('/api/isst/agregar_plato_sugerido/<string:usuario>/<string:preparacion>/<path:ingredientes>/<path:cantidad>/<path:calorias>/<int:calorias_total>', methods = ['GET', 'POST'])
-def agregar_plato_sugerido(usuario):
-    return Response()
+@app.route('/api/isst/agregar_plato_sugerido/<string:nombre>/<string:preparacion>/<path:ingredientes>/<path:cantidad>/<path:calorias>/<int:calorias_total>', methods = ['GET', 'POST'])
+def agregar_plato_sugerido(nombre,preparacion,ingredientes,cantidad,calorias,calorias_total):
+    nombre = nombre.replace('_',' ')
+    preparacion = preparacion.replace('_',' ')
+    ingredientes = ingredientes.replace('_',' ').replace('-','/')
+    cantidad = cantidad.replace('_',' ').replace('-','/')
+    descripcion = descripcion.replace('_',' ').replace('-','/')
+    calorias = calorias.replace('_',' ').replace('-','/')
+    entry = Platos_Sugeridos(nombre = nombre, preparacion = preparacion, ingredientes = ingredientes, cantidad = cantidad, calorias = calorias, calorias_total = calorias_total)
+    db.session.add(entry)
+    db.session.commit()
+    return Response(None,200)
+
+@app.route('/api/isst/obtener_plato_sugerido/<path:calorias_total>', methods = ['GET', 'POST'])
+def obtener_plato_sugerido(calorias_total):
+    calorias_total = calorias_total.split('_')
+    elems = Platos_Sugeridos.query.filter(Platos_Sugeridos.calorias_total > calorias_total[0], Platos_Sugeridos.calorias_total < calorias_total[1]).all()
+    resp = {}
+    num = 0
+    for elem in elems:
+        resp[str(num)] = [elem.nombre,elem.preparacion,elem.ingredientes,elem.cantidad,elem.calorias,elem.calorias_total]
+        num += 1
+    return Response(json.dumps(resp),200)
 
 if __name__ == "__main__":
     bbdd_init()
