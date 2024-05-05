ece27a6a4ddeecfbad438e88ef843bc48eacabcf
Rodrigo Menéndez Muñoz <rodmenm@gmail.com>
2024-05-05 21:56:23 +0200

    premium_change

--- server/routes/index_premium.js
+++ server/routes/index_premium.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { educacion, specops, verreunion } from "./../controllers/mainController.js";
+import { educacion, specops, verreunion, platogen } from "./../controllers/mainController.js";
 
 const router = express.Router();
 
-router.get("/premium/educacion", educacion); // DEBERIA SER SOLO PARA PREMIUM // COMPLETAR
+router.get("/premium/educacion", educacion); 
 
-router.get("/premium/especialistas", specops); // DEBERIA SER SOLO PARA PREMIUM // COMPLETAR
+router.get("/premium/especialistas", specops); 
 
 router.get("/premium/reunion/:dia(\\d+)", verreunion);
+
+router.get("/premium/plato/gen", platogen);
 
 export default router;
