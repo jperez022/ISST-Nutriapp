5e1b081904fad57d3e0cee7efad11bbe657c46cb
Rodrigo Menéndez Muñoz <rodmenm@gmail.com>
2024-05-06 05:34:41 +0200

    Fix

--- server/routes/index_protected.js
+++ server/routes/index_protected.js
@@ -62,7 +62,7 @@ async function incrementK() {
 
 const storage = multer.diskStorage({
   destination: async function (req, file, cb) {
-    cb(null, path.join(projectDirectory, "..", "public", "images", "profilePics"));
+    cb(null, path.join(projectDirectory, "..", "public", "images"));
   },
   filename: async function (req, file, cb) {
     const uniqueSuffix = await getk();
