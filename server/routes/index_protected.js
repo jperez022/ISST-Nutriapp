3b6f7a57328dd75e4a707306358c39f21249cc87
Rodrigo Menéndez Muñoz <rodmenm@gmail.com>
2024-05-06 04:57:00 +0200

    Testing

--- server/routes/index_protected.js
+++ server/routes/index_protected.js
@@ -1,5 +1,8 @@
 import express from "express";
-
+import { fileURLToPath } from "url";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 
 import {
   inicio,
@@ -23,7 +26,52 @@ import {
   objchan,
   segsav,
   segsaveini,
+  chanfoto,
 } from "./../controllers/mainController.js";
+
+const fileName = "auxiliar.txt";
+
+async function getk() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error al leer el archivo", err);
+        reject(err);
+        return;
+      }
+      const k = parseInt(data) || 1; // Si el archivo está vacío o no contiene un número válido, se usa 1 como valor predeterminado
+      resolve(k);
+    });
+  });
+}
+
+async function incrementK() {
+  try {
+    let k = await getk();
+    k++;
+    await fs.promises.writeFile(fileName, k.toString());
+    console.log(`Valor de k incrementado a ${k} y guardado en el archivo`);
+  } catch (error) {
+    console.error("Error al incrementar k y guardar en el archivo", error);
+  }
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const projectDirectory = path.resolve(__dirname);
+
+const storage = multer.diskStorage({
+  destination: async function (req, file, cb) {
+    cb(null, path.join(projectDirectory, "public", "images", "profilePics"));
+  },
+  filename: async function (req, file, cb) {
+    const uniqueSuffix = await getk();
+    await incrementK()
+    cb(null, file.fieldname + "" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
@@ -44,6 +92,8 @@ router.get("/objetivos", objetivos);
 router.get("/perfil", perfil);
 
 router.get("/plato", plato);
+
+app.post("/perfil/foto", upload.single("perfil"), chanfoto);
 
 router.post("/plato/gen/fil", platogenfil);
 
