b793ceaebe92dd3771465d5116e3a3dc7d270cdf
jperez022 <jorge.perez.pineiro@gmail.com>
2024-05-06 03:32:34 +0200

    Some aux files 2

--- server/private_scripts/myaux.py
+++ server/private_scripts/myaux.py
@@ -4,7 +4,7 @@ mydb = mysql.connector.connect(host="localhost",user="root",passwd="jperez022")
 
 my_cursor = mydb.cursor()
 
-my_cursor.execute("CREATE DATABASE isst")
+my_cursor.execute("CREATE DATABASE isst2")
 
 my_cursor.execute("SHOW DATABASES")
 for db in my_cursor:
